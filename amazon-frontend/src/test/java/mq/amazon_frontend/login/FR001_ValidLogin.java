a3cb44c8e1e3d67237f3f761fe6c5831930648c2
minhajul quraishi <minhazq@gmail.com>
2016-07-25 15:31:29 -0400

    Created Controller and Model classes

--- amazon-frontend/src/test/java/mq/amazon_frontend/login/FR001_ValidLogin.java
+++ amazon-frontend/src/test/java/mq/amazon_frontend/login/FR001_ValidLogin.java
@@ -2,10 +2,15 @@ package mq.amazon_frontend.login;
 
 import org.testng.annotations.Test;
 
-public class FR001_ValidLogin {
+import mq.amazon_frontend.framework.Scriptbase;
+
+public class FR001_ValidLogin extends Scriptbase{
 
 	@Test
 	public void validLoginTest(){
+		
+		amazon().homePage().clickLogin().enterEmail("test");
+		
 		System.out.println("Valid login is testing..");
 	}
 }
